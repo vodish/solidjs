f2b264adf06b9927523609739b548cd12266e3f9
pkarasev <p.karasev@psw.ru>
2024-02-16 09:59:27 +0300

    upd

--- src/store/ingrediens.ts
+++ src/store/ingrediens.ts
@@ -3,25 +3,55 @@ import { TIngredient, TIngredietsFetch } from "../utils/types";
 import { createEffect, createResource } from "solid-js";
 
 
-export const [ingredients, setIngredients] = createStore<TIngredient[]>([])
+export const [ingredients, setIngredients]  =   createStore<TIngredient[]>([])
 
+
+// ресурс без параметров
 
 export function ingredientsResource() {
 
-    const [ request ] = createResource<TIngredietsFetch>( async ()=>{
+    const [ request ] = createResource<TIngredietsFetch>(
+        async ()=>{
         const res = await fetch('https://norma.nomoreparties.space/api/ingredients')
         return res.json()
-    })
+        }
+    )
 
     createEffect(()=> {
         // console.log( request.state )
-        
         if ( request.state == 'ready' ) {
             setIngredients( request()?.data )
         }
-        
     })
 
-    return request
+    return request;
 }
+
+
+
+// ресурс с параметрами
+
+export function ingredientsResourceParam() {
+
+
+    async function requestFetch(userId: number) {
+        console.log(userId)
+        const res = await fetch('https://norma.nomoreparties.space/api/ingredients')
+        return res.json()
+    }
+
+
+    const v1 = 12344444;
+    const [ request ] = createResource<TIngredietsFetch, number>(v1, requestFetch)
+
+    createEffect(()=> {
+        // console.log( request.state )
+        if ( request.state == 'ready' ) {
+            setIngredients( request()?.data )
+        }
+    })
+
+    return request;
+}
+
 
