0202244ec2cc871c0c570713f3a774bf2959c165
pkarasev <p.karasev@psw.ru>
2024-05-03 13:47:14 +0300

    upd

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-/* @refresh reload */
 import { render } from 'solid-js/web';
 
 import './index.css';
@@ -10,4 +9,4 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error('Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?');
 }
 
-render(() => <App />, root as HTMLElement);
+render(() => <App />, root);
